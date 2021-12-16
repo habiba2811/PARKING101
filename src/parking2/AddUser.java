b8bb241c8be406869580403bd349c4d45670c875
habiba <70774942+habiba2811@users.noreply.github.com>
2021-12-16 02:26:54 +0200

    Update AddUser.java

--- src/parking2/AddUser.java
+++ src/parking2/AddUser.java
@@ -1,1 +1,23 @@
+static class Admin extends person {
+        private int SpotCode;
 
+        public void AddUser(String UserName, String Password, int RoleID, int shiftID) {
+            File file = new File("AddUser.txt");
+            FileWriter fr = null;
+            try {
+                fr = new FileWriter(file, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fr.write(RoleID + " " + UserName + " " + Password + " " + shiftID + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
